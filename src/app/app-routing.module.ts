8ae8075d32c4b531aa591fb5c4b8d9be615fa413
Borja Quiñones <borja26205@gmail.com>
2025-03-05 19:57:08 +0100

    Header acabado con los enlaces y creado todas las páaginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
+import { PartidasComponent } from './components/partidas/partidas.component';
+import { TarifasComponent } from './components/tarifas/tarifas.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'partidas',
+    component: PartidasComponent
+  },
+  {
+    path: 'tarifas',
+    component: TarifasComponent
+  },
+  {
+    path: 'galeria',
+    component: GaleriaComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
+import { PartidasComponent } from './components/partidas/partidas.component';
+import { TarifasComponent } from './components/tarifas/tarifas.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { ErrorComponent } from './components/error/error.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    PartidasComponent,
+    TarifasComponent,
+    GaleriaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,

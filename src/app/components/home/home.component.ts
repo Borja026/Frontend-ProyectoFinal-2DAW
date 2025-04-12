cf21ae2ff60296f4e793a0a5d9041d4b5c965235
Borja Quiñones <borja26205@gmail.com>
2025-04-12 19:36:30 +0200

    Home acabado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  subMenuVisible: boolean = false;
 
+  toggleSubMenu(event: MouseEvent) {
+    this.subMenuVisible = !this.subMenuVisible;
+
+    let element = event.currentTarget as HTMLElement;
+    let subMenu = element.getElementsByTagName('ul')[0];
+    console.log(subMenu);
+    subMenu.style.display = this.subMenuVisible ? 'block' : 'none';
+  }
 }
